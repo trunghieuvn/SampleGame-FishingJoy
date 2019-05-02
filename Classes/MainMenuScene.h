e707de5c801ff4b9f64df3ce37d183caf294c1d2
Trần Trung Hiếu <hieu.trantrung1204@icloud.com>
2019-05-02 16:52:08 +0700

    + add source Game Fish

---
+++ Classes/MainMenuScene.h
@@ -0,0 +1,22 @@
+﻿//
+//  MainMenuScene.h
+//  FishJoyMini
+//
+//  Created by Ken on 14-3-6.
+//
+//
+#include "cocos2d.h"
+
+#ifndef FishJoyMini_MainMenuScene_h
+#define FishJoyMini_MainMenuScene_h
+USING_NS_CC;
+
+class MainMenuScene : public Scene
+{
+public:
+
+	bool init();
+	CREATE_FUNC(MainMenuScene);
+};
+
+#endif
